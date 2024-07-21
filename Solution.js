092c9b71a5b225ceb1039abf8664a78775853ac1
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-21 14:14:00 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,23 @@
+
+/**
+ * @param {string} input
+ * @return {number}
+ */
+var minimumLength = function (input) {
+    const ALPHABET_SIZE = 26;
+    const ASCII_SMALL_CASE_A = 97;
+    const frequency = new Array(ALPHABET_SIZE).fill(0);
+    for (let i = 0; i < input.length; ++i) {
+        ++frequency[input.codePointAt(i) - ASCII_SMALL_CASE_A];
+    }
+
+    let minLengthAfterOperations = 0;
+    for (let i = 0; i < ALPHABET_SIZE; ++i) {
+        if (frequency[i] === 0) {
+            continue;
+        }
+        minLengthAfterOperations += 2 >> (frequency[i] % 2);
+    }
+
+    return minLengthAfterOperations;
+};
